9ea9288d465528d918af4be1f6d60ae3c9a93ffb
bzs-academy <58351294+bzs-academy@users.noreply.github.com>
2020-02-16 07:04:18 +0100

    2. Lesson

--- main.js
+++ main.js
@@ -1,24 +1,69 @@
 class MyFirstClass{
 
-    constructor(sayi1,sayi2){
-        this.a=sayi1;
-        this.sayi2=sayi2;
+    constructor(number1,number2){
+        this.a=number1;
+        this.number2=number2;
     }
 
-    topla(){
-        console.log(this.a+this.sayi2);
+    topla(number3){
+        console.log(this.a+this.number2+number3);
     }
 
 }
 
+
+
 let myFClass = new MyFirstClass(3,2);
-myFClass.topla();
+myFClass.topla(2);
 
 let myFClass2 = new MyFirstClass(1,2);
 myFClass2.topla();
 
 
-function topla(sayi1,sayi2){
-    sayi1+sayi2;
-    a=sayi1;
+function sum(number1,number2){
+    return (number1+number2);
 }
+
+console.log(sum(1,2));
+
+//Inheritance
+class CoffeeMachine {
+
+    constructor(brand, status){
+        this.brand = brand;
+        this.status = status;
+    }
+
+    powerSwitch(){
+        return(`${this.brand} coffee machine is ${this.status}`);
+    }
+
+}
+
+
+class EspressoMachine extends CoffeeMachine {
+
+    steam(){
+        
+        return(super.powerSwitch() + ' Coffee with steam');
+    }
+
+    powerSwitch(){
+        alert(`${this.brand} espresso machine is ${this.status}`);
+    }
+
+}
+
+let espresso = new EspressoMachine('BZS', 'on');
+alert(espresso.steam());
+//espresso.powerSwitch();
+
+
+let coffeMachine = new CoffeeMachine('Tefal', 'on');
+
+alert(coffeMachine.powerSwitch());
+//Uncaught TypeError: coffeMachine.steam is not a function
+//alert(coffeMachine.steam());
+
+
+
